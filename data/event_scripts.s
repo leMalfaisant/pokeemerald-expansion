4576831fa520e9fc9d124e60741005388bd0a967
malfaisant <97566764+leMalfaisant@users.noreply.github.com>
2024-06-23 11:16:23 -0400

    VARIOUS MAP CHANGES
    -sandy cave mapped, but warps not made
    -removed plusle and minun from route, needs to be removed from dex
    -added tyranitar line encounters in rock smashes at b2f of sandy cavern
    -added morellul (shiinotic prevo) to
    -added nidoran lines encounter at route 110
    TODO:
    -remove plusle and minun from dex
    -add tyranitar line to dex
    -add nidoran line to dex, maybe consider adding more encounters?
    -reconsider morelull location + add to dex if yes
    terrible feeling im forgetting about something but this is fine

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1074,3 +1074,11 @@ EventScript_VsSeekerChargingDone::
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+
+	.include "data/maps/SandyCavern1F/scripts.inc"
+
+	.include "data/maps/SandyCavern_B1F/scripts.inc"
+
+	.include "data/maps/SandyCavern_2F/scripts.inc"
+
+	.include "data/maps/SandyCavern_B2F/scripts.inc"
